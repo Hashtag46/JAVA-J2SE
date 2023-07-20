a6a4b0f5fff80908b8e442906470a647d57307b5
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-07-20 20:10:07 +0530

    Update search.java

--- search.java
+++ search.java
@@ -1,5 +1,5 @@
-public class search {
-    public static void main(String ars[])
+public class Search {
+    public static void main(String[] args)
     {
         int a=Integer.parseInt(ars[0]);
         int b=Integer.parseInt(ars[1]);
