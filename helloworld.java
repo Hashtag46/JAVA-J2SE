9869ef244a9b53cc6f9529c88509c1cb13af847b
Avik Agarwala <98759608+AvikAgarwala@users.noreply.github.com>
2023-07-20 20:08:58 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
-class helloworld
+class Helloworld
 {
-    public static void main(String ars[])
+    public static void main(String[] args)
     {
         System.out.println("Hello World");
     }
